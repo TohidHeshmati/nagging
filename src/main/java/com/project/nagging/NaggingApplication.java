9a4e4f5cd2168243d2bdb56833ff8347a3002e65
tohid <tohidfla@gmail.com>
2020-06-23 02:36:15 +0200

    initialize project

---
+++ src/main/java/com/project/nagging/NaggingApplication.java
@@ -0,0 +1,13 @@
+package com.project.nagging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaggingApplication.class, args);
+	}
+
+}
